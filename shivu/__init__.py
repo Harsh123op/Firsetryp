e88c6caaa9c119a73693ca99405a7994642e55a7
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-06-23 11:39:02 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -23,7 +23,7 @@ OWNER_ID = '6867197197'
 sudo_users = ["7404586329", "6090374191", "6867197197", "6867197197", "6867197197"]
 GROUP_ID = "-1002079121146"
 TOKEN = "7486770290:AAEoAwYTlt-Hq0iZ3nzHKBPc71U7jJBPiF4"
-mongo_url = "mongodb+srv://harshmanjhi180:harsh2279@cluster0.z1pajuv.mongodb.net/?retryWrites=true&w=majority"
+mongo_url = "mongodb+srv://waifu12:waifu12@cluster0.z1pajuv.mongodb.net/?retryWrites=true&w=majority"
 PHOTO_URL = ["https://telegra.ph/file/aa06eb4b312f456e1fd28.jpg", "https://telegra.ph/file/aa06eb4b312f456e1fd28.jpg"]
 SUPPORT_CHAT = "@dark_dream_world"
 UPDATE_CHAT = "@dark_dream_world"
@@ -42,7 +42,7 @@ image_urls = [
     "https://telegra.ph/file/badce7884ce06e92cedb5.jpg"
 ]
 
-DESTINATION_DB_URL = "mongodb+srv://harshmanjhi180:harsh2279@cluster0.z1pajuv.mongodb.net/?retryWrites=true&w=majority"
+DESTINATION_DB_URL = "mongodb+srv://waifu12:waifu12@cluster0.z1pajuv.mongodb.net/?retryWrites=true&w=majority"
 
 
 lol2 = AsyncIOMotorClient(mongo_url)
